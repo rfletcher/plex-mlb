425d2e52294f547e955e4e3288fdbbf712ab541b
Rick Fletcher <fletch@pobox.com>
2009-04-16 10:42:41 -0700

    Added abbreviated team names

--- src/bundle/Code/Classes/Team.py
+++ src/bundle/Code/Classes/Team.py
@@ -1,13 +1,15 @@
 class Team:
-  def __init__(self, teamObj=None, name=None, city=None, id=None):
+  def __init__(self, teamObj=None, abbrev=None, name=None, city=None, id=None):
     if teamObj:
       name = teamObj['name']
       id = teamObj['id']
       city = teamObj['city']
+      abbrev = teamObj['abbrev']
 
     self.id = id
     self.name = name
     self.city = city
+    self.abbrev = abbrev
 
   ############################################################################
   def fullName(self):
