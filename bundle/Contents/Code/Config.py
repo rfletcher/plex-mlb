b44489eb2e465a5101f4c119b8bb08a0a18c3c5e
Rick Fletcher <fletch@pobox.com>
2010-03-12 00:32:29 -0800

    Read environment from the "env" file

--- bundle/Contents/Code/Config.py
+++ bundle/Contents/Code/Config.py
@@ -3,7 +3,11 @@ import os
 import yaml
 
 config_file_path = os.path.dirname(__file__) + '/../config.yml'
-env = "development" if os.path.dirname(__file__) + "/development" else "release"
+
+try:
+  env = open(os.path.dirname(__file__) + '/../env', 'r').read()
+except IOError:
+  env = 'release'
 
 C = yaml.load(open(config_file_path, 'r').read())[env]
 
