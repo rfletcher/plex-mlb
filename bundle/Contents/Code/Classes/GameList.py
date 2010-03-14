41bd600168f99c6bab270e2db5063a2109f575b2
Rick Fletcher <fletch@pobox.com>
2010-03-14 16:39:16 -0700

    Better game list sorting

--- bundle/Contents/Code/Classes/GameList.py
+++ bundle/Contents/Code/Classes/GameList.py
@@ -9,6 +9,39 @@ from Code import Util
 from Code.Config import C
 from Code.Classes import Game
 
+def compare(a, b, PrioritizeFavorites=True, PrioritizeInProgress=True):
+  """
+  Sort the game list in the same fashion as mlb.com (by start time, with
+  complete games at the bottom).  Additionally, move favorite team's game(s) to
+  the top.
+  """
+  # move favorite team's game(s) to the top
+  if PrioritizeFavorites:
+    if a.isFavorite() and b.isFavorite():
+      return compare(a, b, PrioritizeFavorites=False, PrioritizeInProgress=PrioritizeInProgress)
+    if a.isFavorite():
+      return -1
+    if b.isFavorite():
+      return 1
+  
+  # next, in progress-games
+  if PrioritizeInProgress:
+    if a.isInProgress() and b.isInProgress():
+      return compare(a, b, PrioritizeFavorites=PrioritizeFavorites, PrioritizeInProgress=False)
+    if a.isInProgress():
+      return -1
+    if b.isInProgress():
+      return 1
+  
+  # finally, sort by start time
+  if a.time < b.time:
+    return -1
+  elif a.time > b.time:
+    return 1
+  
+  return 0
+  
+
 class GameList(list):
   """
   A list of one day's games
@@ -18,7 +51,7 @@ class GameList(list):
     Load the list of games for a specific date from mlb.com
     """
     self.load(date, streams)
-    self.sort()
+    self.sort(compare)
   
   def load(self, date, streams):
     """
@@ -29,9 +62,5 @@ class GameList(list):
       game = Game.fromXML(xml)
       game.streams = streams[game.event_id] if game.event_id else {}
       self.append(game)
-    
-    # move favorite team's game(s) to the top
-    for i, game in enumerate(self):
-      if game.home_team.isFavorite() or game.away_team.isFavorite():
-        self.insert(0, self.pop(i))
   
+
