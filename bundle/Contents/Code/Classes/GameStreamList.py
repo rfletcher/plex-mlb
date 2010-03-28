0a49e3a9fd8d7e20d658d7348c0e867166046e3a
Rick Fletcher <fletch@pobox.com>
2010-03-27 17:23:46 -0700

    Added Stream.getBest() to automatically pick the best available stream

--- bundle/Contents/Code/Classes/GameStreamList.py
+++ bundle/Contents/Code/Classes/GameStreamList.py
@@ -10,6 +10,26 @@ class GameStreamList(list):
     self.extend(streams)
     self.sort(compare)
   
+  def getBest(self):
+    """
+    Find the best stream in the list
+    """
+    best = None
+    priority = [None, 'audio', 'away', 'basic', 'national', 'home', None, None, None, 'video']
+    
+    for stream in self:
+      weight = 0
+      if stream.type == 'home' and self.game.home_team.isFavorite() or \
+         stream.type == 'away' and self.game.away_team.isFavorite():
+        weight += 4
+      try:
+        weight += priority.index(stream.type)
+        weight += priority.index(stream.kind)
+      except: pass
+      if not best or weight > best['weight']:
+        best = {'stream': stream, 'weight': weight}
+    
+    return best['stream']
 
 def compare(a, b, field=0):
   fields = ['national', 'away', 'home', 'video', 'audio', 'basic', 'condensed', 'highlights']
